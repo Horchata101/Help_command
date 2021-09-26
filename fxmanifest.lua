9bfa682d9d1d54f4d2a07f3a30db137eaf930e5b
Tjaye101 <83477654+Tjaye101@users.noreply.github.com>
2021-09-25 18:20:19 -0700

    Code Added

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,16 @@
 --- Resource Metadata
 fx_version 'cerulean'
-games { 'gta5' } --- Do not touch!
+games { 'gta5' } --- Do not touch! Can mess up the script 
 
 author 'Tjaye#0994' --- Discord, Only contact me if your pull request is not being ansewrd; Other then that use this as your last resort. Thank you and enjoy!
+
+ui_page "htm/index.html"
+
+client_script "main.lua"
+
+files {
+    "html/index.html"
+    "html/style.css"
+    "html/reset.css"
+    "html/listener.js"
+}
