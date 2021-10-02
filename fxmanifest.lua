a5316fc45cc13e1c128a4313e11220b04f3dffac
Horchata <83477654+Horchata101@users.noreply.github.com>
2021-10-02 05:58:04 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,7 @@
 --- Resource Metadata
 fx_version 'cerulean'
-games { 'gta5' } --- Do not touch! Can mess up the script 
+games { 'gta5' }
 
 author 'Tjaye#0994' --- Discord, Only contact me if your pull request is not being ansewrd; Other then that use this as your last resort. Thank you and enjoy!
-
-
 
 client_script "main.lua"
