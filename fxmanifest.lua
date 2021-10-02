f6f93c7de33687b46a85f527951df7e3c969f527
Horchata <83477654+Horchata101@users.noreply.github.com>
2021-10-02 05:57:04 -0700

    Changed to a command script

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,12 +4,6 @@ games { 'gta5' } --- Do not touch! Can mess up the script
 
 author 'Tjaye#0994' --- Discord, Only contact me if your pull request is not being ansewrd; Other then that use this as your last resort. Thank you and enjoy!
 
-ui_page "htm/ui.html"
+
 
 client_script "main.lua"
-
-files {
-    "html/*.html",
-    "html/*.css",
-    "html/*.js",
-  }
--- main.lua
+++ main.lua
@@ -1,37 +1,3 @@
-local oxygen = 100
-
-CreateThread(funtion()
-    while true do
-        Wait(200)
-        TriggerEvent('esx_status:getStatus', 'thirst', function(status)
-            thirst = status.getPercent()
-        end)
-        Wait(200)
-        TriggerEvent('esx_status:getStatus', 'hunger', function(status)
-            hunger = status.getPercent()
-        end)
-        Wait(200)
-    end
-    Wait(200)
+RegisterCommand("help", function()
+    msg("Seems Like you need help? Join our discord:")
 end)
-
-oxygen = math.ceil(GetPlayerUnderwaterTimeRemaining(PlayerId))) * 4
-
-CreateThread(function()
-    while true do
-        Wait(50)
-        local _p = PlayerPedId()
-        SendNUIMessage({
-            action = 'tick',
-            health = (GetEntityHealth(_p)-100),
-            armour = GetPedArmour(_p),
-            stamina = 100 - GetPlayerSprintStaminaRemaining(PlayerPedId()),
-            ooxygen = oxygen,
-            swim = swim,
-            hunger = hunger,
-            thirst = thirst
-
-
-        })
-    end
-end) 
