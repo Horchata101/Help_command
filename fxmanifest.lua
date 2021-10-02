7918bcdaa282b8640e5ea9b97a0f4ce14d7082e9
Horchata <83477654+Horchata101@users.noreply.github.com>
2021-10-02 06:01:51 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,4 +4,5 @@ games { 'gta5' }
 
 author 'Tjaye#0994' --- Discord, Only contact me if your pull request is not being ansewrd; Other then that use this as your last resort. Thank you and enjoy!
 
-client_script "main.lua"
+client_script "client.lua"
+server_script "server.lua"
